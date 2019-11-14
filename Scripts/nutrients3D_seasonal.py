171155e91e515888180ea7b0ed13509c0481798e
Simona Meiler <meilers@dhcp-18-21-163-194.dyn.mit.edu>
2019-11-14 17:01:15 -0500

    bug found and corrected.

--- Scripts/nutrients3D_seasonal.py
+++ Scripts/nutrients3D_seasonal.py
@@ -34,107 +34,70 @@ months_list = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov'
 mon_list = ['26160','26400','26640','26880','27120','27360','27600','27840','28080','28320','28560','28800']
 
 #timevector = np.zeros((1,1))
-NH4_timeseries = np.zeros((len(months_vec),6,160,360))
-NO2_timeseries = np.zeros((len(months_vec),6,160,360))
-NO3_timeseries = np.zeros((len(months_vec),6,160,360))
-PO4_timeseries = np.zeros((len(months_vec),6,160,360))
-FeT_timeseries = np.zeros((len(months_vec),6,160,360))
-S_DIN_timeseries = np.zeros((len(months_vec),6,160,360))
-S_PO4_timeseries = np.zeros((len(months_vec),6,160,360))
-S_Fe_timeseries = np.zeros((len(months_vec),6,160,360))
+# create empty arrays to next fill with the nutrient data from the model
+NH4 = np.zeros((len(months_vec),6,160,360))
+NO2 = np.zeros((len(months_vec),6,160,360))
+NO3 = np.zeros((len(months_vec),6,160,360))
+PO4 = np.zeros((len(months_vec),6,160,360))
+FeT = np.zeros((len(months_vec),6,160,360))
+S_DIN = np.zeros((len(months_vec),6,160,360))
+S_PO4 = np.zeros((len(months_vec),6,160,360))
+S_Fe = np.zeros((len(months_vec),6,160,360))
 
-# Open dataset
+# Open dataset and load values for each nutrient (top 100m --> 0:6 of depth dimension)
 i = 0
 for month in months_vec:
-    data = xr.open_dataset('/Users/meilers/MITinternship/Data/Nutr_mon/nutr_tend.00000'+str(mon_list[month])+'.nc')
-    NH4_timeseries[month,:,:,:] = data.gDAR04.values[:,0:6,:,:]
-    NO2_timeseries[month,:,:,:] = data.gDAR03.values[:,0:6,:,:]
-    NO3_timeseries[month,:,:,:] = data.gDAR02.values[:,0:6,:,:]
-    PO4_timeseries[month,:,:,:] = data.gDAR05.values[:,0:6,:,:]
-    FeT_timeseries[month,:,:,:] = data.gDAR06.values[:,0:6,:,:]
-    S_DIN_timeseries[month,:,:,:] = data.S_DIN.values[:,0:6,:,:]
-    S_PO4_timeseries[month,:,:,:] = data.S_PO4.values[:,0:6,:,:]
-    S_Fe_timeseries[month,:,:,:] = data.S_Fe.values[:,0:6,:,:]
+    data = xr.open_dataset('/Users/meilers/MITinternship/Data/Nutrients_monthly/nutr_tend.'+str(months_list[month])+'.nc')
+    NH4[month,:,:,:] = data.Tp_gTr04.values[:,0:6,:,:]
+    NO2[month,:,:,:] = data.Tp_gTr03.values[:,0:6,:,:]
+    NO3[month,:,:,:] = data.Tp_gTr02.values[:,0:6,:,:]
+    PO4[month,:,:,:] = data.Tp_gTr05.values[:,0:6,:,:]
+    FeT[month,:,:,:] = data.Tp_gTr06.values[:,0:6,:,:]
+    S_DIN[month,:,:,:] = data.S_DIN.values[:,0:6,:,:]
+    S_PO4[month,:,:,:] = data.S_PO4.values[:,0:6,:,:]
+    S_Fe[month,:,:,:] = data.S_Fe.values[:,0:6,:,:]
     data.close()
-    print('Month: '+str(month))
+    print('Month: '+str(month)) #as control if loop works
     i += 1
 
 
-#%% Read in diazotroph data from model
+#%% Read in diazotroph data from model - same setup as for nutrients
+    # 5 diazotroph species. according to Steph TRAC30 to TRAC34
 
-diaz1_timeseries = np.zeros((len(months_vec),6,160,360))
-diaz2_timeseries = np.zeros((len(months_vec),6,160,360))
-diaz3_timeseries = np.zeros((len(months_vec),6,160,360))
-diaz4_timeseries = np.zeros((len(months_vec),6,160,360))
-diaz5_timeseries = np.zeros((len(months_vec),6,160,360))
+diaz1 = np.zeros((len(months_vec),6,160,360))
+diaz2 = np.zeros((len(months_vec),6,160,360))
+diaz3 = np.zeros((len(months_vec),6,160,360))
+diaz4 = np.zeros((len(months_vec),6,160,360))
+diaz5 = np.zeros((len(months_vec),6,160,360))
 
 # Open dataset
 i = 0
 for month in months_vec:
     diaz = xr.open_dataset('/Users/meilers/MITinternship/Data/Diaz_mon/3d.00000'+str(mon_list[month])+'.nc')
-    diaz1_timeseries[month,:,:,:] = diaz.TRAC30.values[:,0:6,:,:]
-    diaz2_timeseries[month,:,:,:] = diaz.TRAC31.values[:,0:6,:,:]
-    diaz3_timeseries[month,:,:,:] = diaz.TRAC32.values[:,0:6,:,:]
-    diaz4_timeseries[month,:,:,:] = diaz.TRAC33.values[:,0:6,:,:]
-    diaz5_timeseries[month,:,:,:] = diaz.TRAC34.values[:,0:6,:,:]
+    diaz1[month,:,:,:] = diaz.TRAC30.values[:,0:6,:,:]
+    diaz2[month,:,:,:] = diaz.TRAC31.values[:,0:6,:,:]
+    diaz3[month,:,:,:] = diaz.TRAC32.values[:,0:6,:,:]
+    diaz4[month,:,:,:] = diaz.TRAC33.values[:,0:6,:,:]
+    diaz5[month,:,:,:] = diaz.TRAC34.values[:,0:6,:,:]
     diaz.close()
     print('Month: '+str(month))
     i += 1
-    
-#%% Load and condense diazotroph data
-ds = xr.open_dataset('/Users/meilers/MITinternship/Data/MarEDat20130403Diazotrophs.nc',decode_times=False)
+# Sum up diazotroph data into one array
+diaz = diaz1 + diaz2 + diaz3 + diaz4 + diaz5
 
-# extract variables which are needed and convert/integrate
-lon_d = ds['LONGITUDE']
-lat_d = ds['LATITUDE'][10:-10] # to match the latitude of the nutrient data
-
-obs = ds['OBSERVATIONS']
-abund = ds['ABUNDANCE']
-bm = ds['BIOMASS']
-nifH = ds['nifHbiom']
-nz_obs = ds['NON_ZERO_OBS']
-nz_abund = ds['NON_ZERO_ABUND']
-nz_bm = ds['NON_ZERO_BIOM']
-nz_nifH = ds['NON_ZERO_nifH']
-
-obs_tot = np.sum(obs[:,0:6,10:-10,:],axis=(0,1))
-abund_tot = np.sum(abund[:,0:6,10:-10,:],axis=(0,1))
-bm_tot = np.sum(bm[:,0:6,10:-10,:],axis=(0,1))
-nifH_tot = np.sum(nifH[:,0:6,10:-10,:],axis=(0,1))
-nz_obs_tot = np.sum(nz_obs[:,0:6,10:-10,:],axis=(0,1))
-nz_abund_tot = np.sum(nz_abund[:,0:6,10:-10,:],axis=(0,1))
-nz_bm_tot = np.sum(nz_bm[:,0:6,10:-10,:],axis=(0,1))
-nz_nifH_tot = np.sum(nz_nifH[:,0:6,10:-10,:],axis=(0,1))
-
-#%% Load and condense nutrient data
-# transport terms
-NH4 = NH4_timeseries
-NO2 = NO2_timeseries
-NO3 = NO3_timeseries
-PO4 = PO4_timeseries
-FeT = FeT_timeseries
-
-# other terms: remineralization and dust in case of Fe
-S_DIN = S_DIN_timeseries
-S_PO4 = S_PO4_timeseries
-S_Fe = S_Fe_timeseries
-
-
-# diazotroph data
-diaz = diaz1_timeseries + diaz2_timeseries + diaz3_timeseries + diaz4_timeseries + diaz5_timeseries
-
-# define constants and dz
-sec = 1  #seconds per year (365.25*86400)
+#%% define sec per year and dz
+sec = 1  #seconds per year (365.25*86400) - not needed; I used it for the annual analysis. 
 dz = [10,10,15,20,20,25] #,35,50,75,100] #(...) dz between two depth layers
 
 #%% sum nutrients up over depth and multiply with corresponding dz and sec
 
-NH4_int = np.zeros((12,6,160,360))
-for i in range(len(dz)):
-    NH4_int[:,i,:,:] = NH4[:,i,:,:]*dz[i]*sec
-#    print(np.max(NH4_int[:,i,:,:]))
-#    print(i)
-NH4_int = np.sum(NH4_int,axis=1)
+NH4_int = np.zeros((12,6,160,360))              #create empty vector
+for i in range(len(dz)):                        #loop over depths                 
+    NH4_int[:,i,:,:] = NH4[:,i,:,:]*dz[i]*sec   #multiply fluxes at each depth with corresponding dz
+    print(np.max(NH4_int[:,i,:,:]))
+    #print(i)
+    #print(dz[i])
+NH4_int = np.sum(NH4_int,axis=1)                #sum up over depth --> new shape of array (12,160,360)
 
 NO2_int = np.zeros((12,6,160,360))
 for i in range(len(dz)):
@@ -176,25 +139,14 @@ for i in range(len(dz)):
     diaz_int[:,i,:,:] = diaz[:,i,:,:]*dz[i]*sec    
 diaz_int = np.sum(diaz_int,axis=1)
 
-#%% Add up the different N species
+#%% Add up the different N species of transport terms
 N_int = NH4_int + NO3_int + NO2_int
-N_int_o = S_DIN_int
-
-#%% Set all negative values to zero
-
-from copy import deepcopy
-N_trans = deepcopy(N_int)
-P_trans = deepcopy(PO4_int)
-Fe_trans = deepcopy(FeT_int)
-N_remin = deepcopy(N_int_o)
-P_remin = deepcopy(S_PO4_int)
-Fe_other = deepcopy(S_Fe_int)
   
-#%% Define transport, remin and total nutrient fluxes; add iron flux
+#%% Define total nutrient fluxes
 
-N_tot = np.add(N_trans,N_remin)
-P_tot = np.add(P_trans,P_remin)
-Fe_tot = np.add(Fe_trans,Fe_other)
+N_tot = np.add(N_int,S_DIN_int)
+P_tot = np.add(PO4_int,S_PO4_int)
+Fe_tot = np.add(FeT_int,S_Fe_int)
 
 #%% Calculate ratios
 
@@ -206,11 +158,11 @@ k = 0.1
 ref_PN = 1.04
 ref_FeN = 1.2
 
-#bio_PN = (P_tot/N_tot)*(1/rpP)
-#bio_FeN = (Fe_tot/N_tot)*(1/rpFe)
+bio_PN = (P_tot/N_tot)*(1/rpP)
+bio_FeN = (Fe_tot/N_tot)*(1/rpFe)
 
-bio_PN = (P_remin/N_remin)*(1/rpP)
-bio_FeN = (Fe_other/N_remin)*(1/rpFe)
+#bio_PN = (P_remin/N_remin)*(1/rpP)
+#bio_FeN = (Fe_other/N_remin)*(1/rpFe)
 
 #%% Make seasonal P:N and Fe:N arrays
 
@@ -222,11 +174,11 @@ bio_PN_DJF = (bio_PN[0,:,:]+bio_PN[1,:,:]+bio_PN[11,:,:])/3
 bio_FeN_DJF = (bio_FeN[0,:,:]+bio_FeN[1,:,:]+bio_FeN[11,:,:])/3
 
 #%% mask where P:N OR Fe:N is sufficient to support diazotrophs
-mask_JJA = np.where((np.mean(bio_FeN[DJF[2],:,:],axis=0) > ref_FeN) & (np.mean(bio_PN[DJF[2],:,:],axis=0) > ref_PN), 1, 0)
+mask_JJA = np.where((np.mean(bio_FeN[2:4,:,:],axis=0) > ref_FeN) & (np.mean(bio_PN[2:4,:,:],axis=0) > ref_PN), 1, 0)
 
 #%% Plot seasonal nutrient ratio patterns
 
-levs_phi = np.linspace(-1.2,-0.8,11)
+levs_phi = np.linspace(0.5,1.5,11)
 
 colmap = plt.get_cmap('RdBu_r')
 
@@ -250,13 +202,13 @@ cbar1 = plt.colorbar(c1,ax=ax[0])
 cbar2 = plt.colorbar(c2,ax=ax[1])
 cbar3 = plt.colorbar(c3,ax=ax[2])
 cbar4 = plt.colorbar(c4,ax=ax[3])
-ax[0].set_title('Seasonal nutrient ratio P:N (remin only)')
+ax[0].set_title('Seasonal nutrient ratio P:N')
 #cbar.set_label(''+str(name_nut[nu])+'',rotation=90, position=(0.5,0.5))
 plt.show()
-#fig.savefig('/Users/meilers/MITinternship/Plots/seasonal_PN_remin.png', bbox_inches='tight', dpi=300)
+fig.savefig('/Users/meilers/MITinternship/Plots/seasonal_PN_77.png', bbox_inches='tight', dpi=300)
 
 #%%
-levs_phi = np.linspace(-2,1,11)
+levs_phi = np.linspace(0.5,1.5,11)
 
 colmap = plt.get_cmap('RdBu_r')
 
@@ -283,7 +235,7 @@ cbar4 = plt.colorbar(c4,ax=ax[3])
 ax[0].set_title('Seasonal nutrient ratio Fe:N')
 #cbar.set_label(''+str(name_nut[nu])+'',rotation=90, position=(0.5,0.5))
 plt.show()
-#fig.savefig('/Users/meilers/MITinternship/Plots/seasonal_FeN_remin.png', bbox_inches='tight', dpi=300)
+fig.savefig('/Users/meilers/MITinternship/Plots/seasonal_FeN_77.png', bbox_inches='tight', dpi=300)
 
 #%% Plot 1 nutrient at 1 depth
 
@@ -349,4 +301,17 @@ for i in range(4):
         #cbar.set_label('more         (m)           less', rotation=90,labelpad=10)
 #fig.savefig('/Users/meilers/MITinternship/Plots/monthly_diaz_anomalies2.png', bbox_inches='tight', dpi=300)
         
-
+#%% Just for some quick plots
+fig,ax = plt.subplots(subplot_kw={'projection':ccrs.PlateCarree(central_longitude=0)},figsize=(12,4))
+ax.coastlines(color='#888888',linewidth=1.5)
+ax.add_feature(cfeature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='none', facecolor=cfeature.COLORS['land']))
+c = ax.contourf(lon,lat,mask_JJA)#,levels=levs[nut],cmap=colmap,extend='both')
+lon_formatter = LongitudeFormatter(zero_direction_label=True)
+lat_formatter = LatitudeFormatter()
+ax.xaxis.set_major_formatter(lon_formatter)
+ax.yaxis.set_major_formatter(lat_formatter)
+ax.set_xticks([0,60,120,180,240,300,360], crs=ccrs.PlateCarree())
+ax.set_yticks([-80, -60, -40, -20, 0, 20, 40, 60, 80], crs=ccrs.PlateCarree())
+cbar = plt.colorbar(c,ax=ax)
+#cbar.set_label(''+str(name_nut[nut])+'',rotation=90, position=(0.5,0.5))
+plt.show()
